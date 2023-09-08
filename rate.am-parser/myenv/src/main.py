72c9e7576c4593f54e29017b2db8b4d701343712
areg <aregsargsyan8888@gmail.com>
2023-09-08 18:30:32 +0400

    tets

--- rate.am-parser/myenv/src/main.py
+++ rate.am-parser/myenv/src/main.py
@@ -1,40 +1,42 @@
-from parser import get_dom
-from bs4 import BeautifulSoup
-import webbrowser
-import os
+# from parser import get_dom
+# from bs4 import BeautifulSoup
+# import webbrowser
+# import os
 
-from simpleServer import run_server
+# from simpleServer import run_server
 
-url = "https://rate.am/"
-parsed_dom = get_dom(url)
+# url = "https://rate.am/"
+# parsed_dom = get_dom(url)
 
-# save enq anum pars exac html taza output.html filum
-with open('output.html', 'w', encoding='utf-8') as file:
-    file.write(parsed_dom.prettify())
+# # save enq anum pars exac html taza output.html filum
+# with open('output.html', 'w', encoding='utf-8') as file:
+#     file.write(parsed_dom.prettify())
 
-# vercnumenq index.html@ vor BeautifulSoup ov domin dimenq
-with open('index.html', 'r') as file:
-    html_content = file.read()
+# # vercnumenq index.html@ vor BeautifulSoup ov domin dimenq
+# with open('index.html', 'r') as file:
+#     html_content = file.read()
 
-# ogtagorcumenq BeautifulSoup lib@ html-i analizi hamar
-soup = BeautifulSoup(html_content, 'html.parser')
+# # ogtagorcumenq BeautifulSoup lib@ html-i analizi hamar
+# soup = BeautifulSoup(html_content, 'html.parser')
 
-# gtnumenq mer uzac@ teg@ te urde pti avelcnenq pars exac html@
-target_tag = soup.find('div', {'id': 'wrapper-for-inserting'})
+# # gtnumenq mer uzac@ teg@ te urde pti avelcnenq pars exac html@
+# target_tag = soup.find('div', {'id': 'wrapper-for-inserting'})
 
-target_tag.clear()
-target_tag.append(BeautifulSoup(parsed_dom.prettify(), 'html.parser'))
+# target_tag.clear()
+# target_tag.append(BeautifulSoup(parsed_dom.prettify(), 'html.parser'))
 
-with open('index.html', 'w') as file:
-    file.write(str(soup))
+# with open('index.html', 'w') as file:
+#     file.write(str(soup))
 
-# vercnumenq tvyal directorian
-current_directory = os.getcwd()
+# # vercnumenq tvyal directorian
+# current_directory = os.getcwd()
 
-full_path_to_index = os.path.join(current_directory, 'index.html')
+# full_path_to_index = os.path.join(current_directory, 'index.html')
 
-# bacumenq index.html@ chrom brauwserum
-webbrowser.get('chrome').open('file://' + full_path_to_index, new=2)
+# # bacumenq index.html@ chrom brauwserum
+# webbrowser.get('chrome').open('file://' + full_path_to_index, new=2)
 
-# server@ runenq anum
-run_server(parsed_dom.prettify())
+# # server@ runenq anum
+# run_server(parsed_dom.prettify())
+
+print("xx")
